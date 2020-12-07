29400c861dcedf8c08ccdac069efe5f2724fd085
TZockt <info@tzockt.de>
2020-12-07 13:55:27 +0100

    Upload Unterricht Stuff

---
+++ Unterricht/StClass.java
@@ -0,0 +1,11 @@
+package Unterricht;
+
+public class StClass
+{
+  static int i; //global und einmal vorhanden
+  public static void stMethode () 
+  {             //global und einmal vorhanden
+      i=9+2;
+  }
+}
+
---
+++ Unterricht/StMainClass.java
@@ -0,0 +1,11 @@
+package Unterricht;
+public class StMainClass{
+    public static void main(String[] args) 
+    {
+        StClass.i=10;
+        StClass.stMethode();
+        System.out.println(StClass.i);
+    }
+        
+    
+}
