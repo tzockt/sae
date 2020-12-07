b882f51363278b5c29c57367c2ebf14152cd8d19
TZockt <info@tzockt.de>
2020-12-07 14:23:57 +0100

    Change

--- Unterricht/StClass.java
+++ Unterricht/StClass.java
@@ -2,7 +2,7 @@ package Unterricht;
 
 public class StClass
 {
-  static int i; //global und einmal vorhanden
+  int i; //global und einmal vorhanden
   public static void stMethode () 
   {             //global und einmal vorhanden
       i=i*2;
--- Unterricht/StMainClass.java
+++ Unterricht/StMainClass.java
@@ -6,6 +6,7 @@ public class StMainClass{
         StClass.stMethode();
         System.out.println(StClass.i);
     }
+    
+    }
         
     
-}
