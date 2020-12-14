70aa635bc27151c065676a34b601632b8f6c51d1
TZockt <info@tzockt.de>
2020-12-14 15:03:50 +0100

    Upload

--- JavaOOP/Aufgabe4/Konto.java
+++ JavaOOP/Aufgabe4/Konto.java
@@ -4,14 +4,19 @@ public class Konto {
     private String kNr;
     private String kInhaber;
     private double kStand;
+    private static int counter=0;
 
     public Konto()
-    { kStand=0; }
+    { kStand=0;
+      counter++;
+     }
 
     public Konto(String kInhaber, String kNr, double kStand)
     { this.kInhaber=kInhaber;
     this.kNr=kNr;
-    this.kStand=kStand;}
+    this.kStand=kStand;
+    counter++;
+    }
 
     public void einzahlen(double betrag) {
         kStand = kStand + betrag;
@@ -24,5 +29,19 @@ public class Konto {
     public double getKStand() {
         return kStand;
     }
-
+    public String getkNr(){
+        return kNr;
+    }
+    public String getKInhaber(){
+        return kInhaber;
+    }
+    public void setkNr (String kNr){
+        this.kNr=kNr;
+    }
+    public void setKInhaber(String kInhaber){
+        this.kInhaber=kInhaber;
+    }
+    public int getCounter(){
+        return counter;
+    }
 }
