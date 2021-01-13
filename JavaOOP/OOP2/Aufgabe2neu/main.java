16aff926ec7856458b27ae3c44b784e36a0f441e
TZockt <info@tzockt.de>
2021-01-13 14:16:00 +0100

    ywet

--- JavaOOP/OOP2/Aufgabe2neu/main.java
+++ JavaOOP/OOP2/Aufgabe2neu/main.java
@@ -5,10 +5,17 @@ public class main {
     public static void main(String[] args) {
         
         rechner pc1 = new rechner("Kevins", "00-00-00-00-00-01","kevin.de","192.168.178.1","Kevin Stadt");
+        rechner pc2 = new rechner("Peter", "00-00-00-00-00-02","peter.de","192.168.178.2","Peter Stadt");
+        rechner pc3 = new rechner("Manuel", "00-00-00-00-00-03","manuel.de","192.168.178.3","Manuel Stadt");
 
+        rechner pc4 = new rechner("Manuel", "00-00-00-00-00-03","manuel.de","192.168.178.3");
 
 
         pc1.ausgabe();
+        pc2.ausgabe();
+        pc3.ausgabe();
+        pc4.ausgabe();
+    
     }
 
 }
