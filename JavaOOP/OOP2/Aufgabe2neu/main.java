8f587dffd4d4689906863f4bb96262739b102208
TZockt <info@tzockt.de>
2021-01-13 13:50:52 +0100

    Upload shit

--- JavaOOP/OOP2/Aufgabe2neu/main.java
+++ JavaOOP/OOP2/Aufgabe2neu/main.java
@@ -2,4 +2,13 @@ package JavaOOP.OOP2.Aufgabe2neu;
 
 public class main {
     
+    public static void main(String[] args) {
+        
+        rechner pc1 = new rechner("Kevins", "00-00-00-00-00-01","kevin.de","192.168.178.1","Kevin Stadt");
+
+
+
+        pc1.ausgabe();
+    }
+
 }
